cd1943b3ddaed3c6bf42258b76839121ba3c70d8
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2023-01-11 16:49:02 +0300

    feature/creating_list     1. added function for register server     2. added function for update user list

--- db_adapater.py
+++ db_adapater.py
@@ -2,15 +2,91 @@ from mysql.connector import connect, Error
 import config
 
 cfg = config.DBConfig()
-try:
-    with connect(
-            host=cfg.db_host,
-            user=cfg.db_user,
-            password=cfg.db_password,
-            database=cfg.db_name,
-    ) as connection:
-        print('Connect successfully')
-        print(connection)
-except Error as e:
-    print('Connect error')
-    print(e)
+
+
+def db_select_one(query):
+    try:
+        with connect(
+                host=cfg.db_host,
+                user=cfg.db_user,
+                password=cfg.db_password,
+                database=cfg.db_name,
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                connection.commit()
+                if len(result) > 0:
+                    return result
+                else:
+                    return None
+    except Error as e:
+        print('Connect error')
+        print(e)
+
+
+def db_create(query):
+    try:
+        with connect(
+                host=cfg.db_host,
+                user=cfg.db_user,
+                password=cfg.db_password,
+                database=cfg.db_name,
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+
+    except Error as e:
+        print('Connect error')
+        print(e)
+
+
+def get_server(server_id):
+    query = 'SELECT id,discord_server_id FROM servers WHERE discord_server_id=' + str(server_id)
+    return db_select_one(query)
+
+
+def create_server(discord_server_id):
+    query = 'INSERT INTO servers (discord_server_id) VALUES (' + str(discord_server_id) + ')'
+    return db_create(query)
+
+
+def get_user(server_id, user_id):
+    query = '''SELECT users.id, users.discord_user_id, users.server_id
+     FROM dcounter.users
+      LEFT JOIN (servers)
+       ON (servers.id=users.server_id)
+        WHERE servers.discord_server_id=''' + str(server_id) + ' AND users.discord_user_id=' + str(user_id)
+    return db_select_one(query)
+
+
+def addUser(server_id, user_id):
+    query = 'INSERT INTO users (discord_user_id,server_id) VALUES (' + str(user_id) + ',' + str(server_id) + ')'
+    return db_create(query)
+
+
+def update_members_guild(server_id, members):
+    server = get_server(server_id)[0]
+    if server != None:
+        for person in members:
+            if get_user(server[1], person.id) == None:
+                addUser(server[0], person.id)
+
+
+def get_nomination(server_id):
+    query = 'SELECT id,name,server_id FROM nominations WHERE server_id=' + str(server_id)
+    return db_select_one(query)
+
+
+def get_nomination_users(nomination_id):
+    query = 'SELECT nomination_id,user_id,count FROM nomination_user WHERE nomination_id=' + str(nomination_id)
+    return db_select_one(query)
+
+
+def get_user_nominations(user_id, server_id):
+    query = '''SELECT servers.discord_server_id, users.discord_user_id, nominations.name, nomination_user.count
+     FROM nomination_user
+      LEFT JOIN(servers,nominations,users) ON (nominations.server_id=servers.id AND nomination_user.nomination_id=nominations.id AND nomination_user.user_id=users.id)
+       WHERE servers.discord_server_id=''' + str(server_id) + ' AND users.discord_user_id=' + str(user_id)
+    return db_select_one(query)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import logging
 import random
 import config
+import db_adapater
 
 cfg = config.DCounterConfig()
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -19,27 +20,45 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('_________________')
-    print(f'Сервер:')
-    print(f'{message.guild.id}')
-    print(f'{message.guild.name}')
-    print(f'Автор:')
-    print(f'{message.author}')
-    print(message.author.id)
-    print(f'Сообщение:')
-    print(f'{message.content}')
-    print('_________________')
+    # print('_________________')
+    # print(f'Сервер:')
+    # print(f'{message.guild.id}')
+    # print(f'{message.guild.name}')
+    # print(f'Автор:')
+    # print(f'{message.author}')
+    # print(message.author.id)
+    # print(f'Сообщение:')
+    # print(f'{message.content}')
+    # print('_________________')
 
     if message.author == client.user:
         return
 
     if message.content.startswith('$random'):
         guild = client.get_guild(message.guild.id)
+        members = guild.members
         number = random.randint(1, message.guild.member_count)
         user = guild.members[number - 1]
         user_id = user.id
-        # await message.channel.send(user+'победитель!')
         await message.channel.send(f"<@{user_id}> is winner")
+
+    if message.content.startswith('$regServer'):
+        server = db_adapater.get_server(message.guild.id)
+
+        if server != None:
+            await message.channel.send("Сервер уже зарегистрирован!")
+        else:
+            db_adapater.create_server(message.guild.id)
+            guild = client.get_guild(message.guild.id)
+            members = guild.members
+            db_adapater.update_members_guild(guild.id, members)
+            await message.channel.send("Сервак зарегистрирован!")
+
+    if message.content.startswith('$regPeople'):
+        guild = client.get_guild(message.guild.id)
+        members = guild.members
+        db_adapater.update_members_guild(guild.id, members)
+        await message.channel.send("Список обновлён")
 
 
 client.run(cfg.token, log_handler=handler)
